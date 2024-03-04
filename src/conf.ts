a2afc26e56eeb88c353460c06d74ba475996ba5d
อิลฮัม หะยีดอเล๊าะ <ihsaniah112@gmail.com>
2024-03-05 01:45:35 +0700

    set

--- src/conf.ts
+++ src/conf.ts
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const conf = {
     isProd,
-    apiPrefix: isProd? 'https://test-deploy-414415.as.r.appspot.com/' : 'http://localhost:1337'
+    apiPrefix: isProd? 'https://godzilla-01.as.r.appspot.com' : 'https://godzilla-01.as.r.appspot.com'
 }
 
 export default conf;

f38a2b7dd31970a7ac664f2cedd6a4ab9c7f5e9c
อิลฮัม หะยีดอเล๊าะ <ihsaniah112@gmail.com>
2024-03-06 03:17:12 +0700

    fix

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -53,7 +53,7 @@ const Home = () => {
 
   const handleDownload = async (imageId: string, imageName: string) => {
     try {
-      const response = await fetch(`${conf.apiPrefix}/${imageId}`, {
+      const response = await fetch(`${imageId}`, {
         method: "GET",
         headers: {
           Authorization: `Bearer ${userData.jwt}`,
